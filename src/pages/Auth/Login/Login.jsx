7eac4ef47fdb29d1a53bc286583117f3a03d7984
Vadim Hruzd <v.hruzd.vironit@gmail.com>
2023-08-07 12:18:43 +0300

    sd

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -4,10 +4,15 @@ import jwtDecode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import { useAuthMutation } from "../../../store/api/AuthApi";
 
-import { LoginSocialFacebook, LoginSocialApple } from "reactjs-social-login";
+import {
+  LoginSocialFacebook,
+  LoginSocialApple,
+  LoginSocialGoogle,
+} from "reactjs-social-login";
 import {
   FacebookLoginButton,
   AppleLoginButton,
+  GoogleLoginButton,
 } from "react-social-login-buttons";
 
 const Login = () => {
@@ -16,49 +21,84 @@ const Login = () => {
   const [auth] = useAuthMutation();
   return (
     <div className={s.login_wrapper}>
-      Battle of the bets
-      <GoogleOAuthProvider clientId="485675376523-a4vgo2kde09v6s93144f8bi3rvu43is7.apps.googleusercontent.com">
-        <GoogleLogin
-          type="icon"
-          onSuccess={(credentialResponse) => {
-            // localStorage.setItem("user", credentialResponse.credential);
-            // console.log(credentialResponse);
-            // console.log(localStorage.getItem("user"));
+      <div className={s.logo}>
+        <img
+          src="https://images.squarespace-cdn.com/content/v1/64725f2836f4e8340893612b/865d048d-03a4-47f4-b776-d5dd0820abd3/BOTB+Logo.png"
+          width={100}
+          alt="nothing"
+        />
+      </div>
+      <div className={s.text}>Login via social networks</div>
+      <div className={s.auth_btns}>
+        {/* <GoogleOAuthProvider clientId="485675376523-a4vgo2kde09v6s93144f8bi3rvu43is7.apps.googleusercontent.com">
+          {/* <GoogleLogin
+            type="icon"
+            onSuccess={(credentialResponse) => {
+              auth(credentialResponse.credential)
+                .unwrap()
+                .then((response) => {
+                  localStorage.setItem("user", response.userId);
+                  window.location.reload();
+                })
+                .catch((error) => console.log(error));
+            }}
+            onError={() => {
+              alert("Login failed");
+            }}
+          /> */}
+        <LoginSocialGoogle
+          className={s.btn}
+          client_id="485675376523-a4vgo2kde09v6s93144f8bi3rvu43is7.apps.googleusercontent.com"
+          onResolve={(credentialResponse) => {
+            console.log(credentialResponse);
             auth(credentialResponse.credential)
               .unwrap()
               .then((response) => {
+                console.log(response);
                 localStorage.setItem("user", response.userId);
                 window.location.reload();
               })
               .catch((error) => console.log(error));
           }}
-          onError={() => {
-            alert("Login failed");
+          onReject={(error) => {
+            console.log(error);
           }}
-        />
-      </GoogleOAuthProvider>
-      <LoginSocialFacebook
-        appId="239859572250126"
-        onResolve={(response) => {
-          console.log(response);
-        }}
-        onReject={(error) => {
-          console.log(error);
-        }}
-      >
-        <FacebookLoginButton />
-      </LoginSocialFacebook>
-      <LoginSocialApple
-        client_id="6455939972"
-        onResolve={(response) => {
-          console.log(response);
-        }}
-        onReject={(error) => {
-          console.log(error);
-        }}
-      >
-        <AppleLoginButton />
-      </LoginSocialApple>
+        >
+          <GoogleLoginButton
+            text=""
+            align="center"
+            preventActiveStyles={true}
+          />
+        </LoginSocialGoogle>
+        <LoginSocialFacebook
+          className={s.btn}
+          appId="239859572250126"
+          onResolve={(response) => {
+            console.log(response);
+          }}
+          onReject={(error) => {
+            console.log(error);
+          }}
+        >
+          <FacebookLoginButton
+            text=""
+            align="center"
+            preventActiveStyles={true}
+          />
+        </LoginSocialFacebook>
+        <LoginSocialApple
+          className={s.btn}
+          client_id="6455939972"
+          onResolve={(response) => {
+            console.log(response);
+          }}
+          onReject={(error) => {
+            console.log(error);
+          }}
+        >
+          <AppleLoginButton text="" align="center" preventActiveStyles={true} />
+        </LoginSocialApple>
+      </div>
     </div>
   );
 };
