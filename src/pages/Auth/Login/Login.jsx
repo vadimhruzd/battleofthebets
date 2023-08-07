29db1199ab875714ae241bebd063229073bb49d6
Vadim Hruzd <v.hruzd.vironit@gmail.com>
2023-08-07 13:46:59 +0300

    sd

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
           client_id="485675376523-a4vgo2kde09v6s93144f8bi3rvu43is7.apps.googleusercontent.com"
           onResolve={(credentialResponse) => {
             console.log(credentialResponse);
-            auth(credentialResponse)
+            auth(credentialResponse.data.access_token)
               .unwrap()
               .then((response) => {
                 console.log(response);
