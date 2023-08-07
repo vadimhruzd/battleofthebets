d6f5182377a54fc1e7dcbf0cfac24d684eb79800
Vadim Hruzd <v.hruzd.vironit@gmail.com>
2023-08-07 15:23:41 +0300

    sd

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
       </div>
       <div className={s.text}>Login via social networks</div>
       <div className={s.auth_btns}>
-        <GoogleOAuthProvider clientId="485675376523-a4vgo2kde09v6s93144f8bi3rvu43is7.apps.googleusercontent.com">
+        {/* <GoogleOAuthProvider clientId="485675376523-a4vgo2kde09v6s93144f8bi3rvu43is7.apps.googleusercontent.com">
           <GoogleLogin
             className={s.btn}
             type="icon"
@@ -48,8 +48,8 @@ const Login = () => {
               alert("Login failed");
             }}
           />
-        </GoogleOAuthProvider>
-        {/* <LoginSocialGoogle
+        </GoogleOAuthProvider> */}
+        <LoginSocialGoogle
           className={s.btn}
           client_id="485675376523-a4vgo2kde09v6s93144f8bi3rvu43is7.apps.googleusercontent.com"
           onResolve={(credentialResponse) => {
@@ -72,7 +72,7 @@ const Login = () => {
             align="center"
             preventActiveStyles={true}
           />
-        </LoginSocialGoogle> */}
+        </LoginSocialGoogle>
         <LoginSocialFacebook
           className={s.btn}
           appId="239859572250126"
